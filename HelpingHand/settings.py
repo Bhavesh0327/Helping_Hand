16201b04f1db2ab910d54328b0f0c26a7b3917a2
aananthv <aananthv@outlook.com>
2020-04-28 17:36:38 +0530

    Remove CommonMiddleware

--- HelpingHand/settings.py
+++ HelpingHand/settings.py
@@ -47,12 +47,11 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    'api.middleware.JsonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'api.middleware.JsonMiddleware'
 ]
 
 ROOT_URLCONF = 'HelpingHand.urls'
